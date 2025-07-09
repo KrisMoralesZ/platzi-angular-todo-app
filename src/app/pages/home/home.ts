08845e14ac1f409fa03e0eb8adc8e4938bcef568
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-09 12:26:20 -0700

    refactor to interface

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,41 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
-  tasks = signal([
-    'Install Angular CLi',
-    'Create Project',
-    'Create Component',
-    'Create Service',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Install Angular CLi',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Create Project',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Create Component',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      title: 'Create Service',
+      completed: false,
+    },
   ]);
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
