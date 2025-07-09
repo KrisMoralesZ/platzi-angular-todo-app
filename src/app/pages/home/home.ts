b1284743e0774ae530835d1e0325a76f048d1368
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-09 12:01:30 -0700

    update and delete todos

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -14,4 +14,16 @@ export class Home {
     'Create Component',
     'Create Service',
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
 }
