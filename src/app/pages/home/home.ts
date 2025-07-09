f63ccf4893a7159ba9e5d7ef20ca8ecfa5845864
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-09 11:48:14 -0700

    display tasks

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home {
-
+  tasks = signal([
+    'Install Angular CLi',
+    'Create Project',
+    'Create Component',
+    'Create Service',
+  ]);
 }
